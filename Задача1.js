cf853717b379139299fc8a36a932492e9983c1df
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2024-02-02 23:27:34 +0300

    Initial commit

--- Задача1.js
+++ Задача1.js
@@ -3,10 +3,10 @@ const findLongestWord = (words) => {
     for (let word of words) {
       if (word.length > longestWord.length) {
         longestWord = word;
-      }
-    }
+      };
+    };
     return longestWord;
-  }
+  };
   
   console.log(findLongestWord(["Java", "Python", "JavaScript"])); // "JavaScript"
   console.log(findLongestWord(["football", "basketball", "tennis"])); // "basketball"
